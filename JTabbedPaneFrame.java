bec36ab65d7bfa3088078bcc6db6091becc45616
Sofia C <91104513@g.mvusd.net>
2021-03-01 19:19:04 +0000

    Initial commit

---
+++ JTabbedPaneFrame.java
@@ -0,0 +1,10 @@
+import javax.swing.JFrame;
+
+public class JTabbedPaneFrame extends JFrame // child class
+{
+  public JTabbedPaneFrame ()
+  {
+    super ("Sofia's JTabbedPane Demo");
+  } // end default and only constructor
+
+} // end JTabbedPaneFrame class
---
+++ Main.java
@@ -0,0 +1,14 @@
+import javax.swing.JFrame;
+
+public class Main
+{
+	public static void main (String[] args)
+	{
+		JTabbedPaneFrame tpf = new JTabbedPaneFrame ();
+    tpf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // IMPORTANT
+    tpf.setSize(450, 400);
+    tpf.setVisible(true);
+    
+	} // end main method
+
+} // end Main class`
